24c0d6e658a14d9ba317a19df03195cc90ab8f16
suman priya <sumanpriya@outlook.in>
2022-09-27 12:49:31 +0530

    attendance management app

---
+++ app/src/main/java/com/example/attend/ClassItem.java
@@ -0,0 +1,40 @@
+package com.example.attend;
+
+public class ClassItem {
+    private String className;
+
+
+    private long cid;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    private String subjectName;
+
+    public ClassItem(long cid, String className, String subjectName) {
+        this.cid = cid;
+        this.className = className;
+        this.subjectName = subjectName;
+    }
+
+    public long getCid() {
+        return cid;
+    }
+
+    public void setCid(long cid) {
+        this.cid = cid;
+    }
+}
